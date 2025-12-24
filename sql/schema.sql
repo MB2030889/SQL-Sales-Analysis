b41b38d1d24cda33addf354d15221b02c77ad93d
MB2030889 <bmylasha@gmail.com>
2025-12-24 13:56:38 -0600

    Create schema.sql
    Creating tables

---
+++ sql/schema.sql
@@ -0,0 +1,28 @@
+-- CREATES TABLES
+CREATE TABLE customers (
+  customer_id INTEGER PRIMARY KEY,
+  name TEXT,
+  email TEXT
+);
+
+CREATE TABLE products (
+  product_id INTEGER PRIMARY KEY,
+  name TEXT,
+  price REAL
+);
+
+CREATE TABLE orders (
+  order_id INTEGER PRIMARY KEY,
+  customer_id INTEGER,
+  order_date TEXT,
+  FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE order_items (
+  order_item_id INTEGER PRIMARY KEY,
+  order_id INTEGER,
+  product_id INTEGER,
+  quantity INTEGER,
+  FOREIGN KEY (order_id) REFERENCES orders(order_id),
+  FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
